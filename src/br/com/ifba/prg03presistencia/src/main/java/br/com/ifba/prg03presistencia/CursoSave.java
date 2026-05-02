d10a5e096d0ff5ef535a10ecc229078fe0d65755
italoinacio-lang <italo.inacio@ifba.edu.br>
2026-05-01 21:22:41 -0300

    Projetos pgrItaloInácio. -Atividade03: Calculadora de Idade. -Atividade04: Gênio Akinetor de Divinhação. -Atividade05: Fatorial dos Numeros na Tela_Principal. -Atividade06: Condigo Simples de Conta corrente e conta poupança. -Atividade07: Encapsulamento com Interfece -Atividade08: Elaborando um Relacionamento entre Entidades -Atividade09: Herança usando Formas 2D e 3D -Projeto03presistencia de Curso

---
+++ src/br/com/ifba/prg03presistencia/src/main/java/br/com/ifba/prg03presistencia/CursoSave.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.prg03presistencia;
+
+/**
+ *
+ * @author Italo
+ */
+public class CursoSave {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
